8fd5fe263d95db37d50e1c66e579a9e0e0beb3a9
desarrollo54 <desarrollo5@aveonline.co>
2021-06-05 15:51:26 -0500

    se apaga la conexion a mongo

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { connectMongo } = require("./core/db/mongo/index");
 const { startGraphQl } = require("./core/graphql/");
 
 function startApp() {
-  connectMongo().then(console.log(`Mogo OK`)).then(startGraphQl());
+  startGraphQl();
+  // connectMongo().then(console.log(`Mogo OK`)).then();
 }
 
 startApp();
